6380d682b4cc4d528a4301284ef1b3a961ac7a44
Valentin Baca <valbaca@gmail.com>
2021-11-22 19:53:31 -0800

    add template files: day0.py and day0.txt

---
+++ advent/year2020/day0.py
@@ -0,0 +1,22 @@
+from advent.elf import read_lines
+import re
+
+
+def part1(lines):
+    pass
+    # code here
+
+
+def part2(lines):
+    pass
+    # code here
+
+
+def main():
+    lines = read_lines(__file__)
+    print(part1(lines))
+    print(part2(lines))
+
+
+if __name__ == '__main__':
+    main()
