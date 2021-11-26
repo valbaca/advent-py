fe3b7bdd8af2081a14c2f53af927b66de93437c6
Valentin Baca <valbaca@gmail.com>
2021-11-26 12:53:38 -0800

    moved combine_phased_rotations to elf

--- advent/year2020/day13.py
+++ advent/year2020/day13.py
@@ -32,63 +32,12 @@ def parse_part2(lines):
     return inputs
 
 
-# Stealing from https://math.stackexchange.com/questions/2218763/how-to-find-lcm-of-two-numbers-when-one-starts-with-an-offset
-
-def combine_phased_rotations(a_period, a_phase, b_period, b_phase):
-    """Combine two phased rotations into a single phased rotation
-
-    Returns: combined_period, combined_phase
-
-    The combined rotation is at its reference point if and only if both a and b
-    are at their reference points.
-    """
-    gcd, s, t = extended_gcd(a_period, b_period)
-    phase_difference = a_phase - b_phase
-    pd_mult, pd_remainder = divmod(phase_difference, gcd)
-    if pd_remainder:
-        raise ValueError("Rotation reference points never synchronize.")
-
-    combined_period = a_period // gcd * b_period
-    combined_phase = (a_phase - s * pd_mult * a_period) % combined_period
-    return combined_period, combined_phase
-
-
-def arrow_alignment(red_len, green_len, advantage):
-    """Where the arrows first align, where green starts shifted by advantage"""
-    period, phase = combine_phased_rotations(
-        red_len, 0, green_len, -advantage % green_len
-    )
-    return -phase % period
-
-
-def extended_gcd(a, b):
-    """Extended Greatest Common Divisor Algorithm
-
-    Returns:
-        gcd: The greatest common divisor of a and b.
-        s, t: Coefficients such that s*a + t*b = gcd
-
-    Reference:
-        https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Pseudocode
-    """
-    old_r, r = a, b
-    old_s, s = 1, 0
-    old_t, t = 0, 1
-    while r:
-        quotient, remainder = divmod(old_r, r)
-        old_r, r = r, remainder
-        old_s, s = s, old_s - quotient * s
-        old_t, t = t, old_t - quotient * t
-
-    return old_r, old_s, old_t
-
-
 def solve_part2(buses):
     all_period, all_phase = 1, 0  # identity
     for phase, period in enumerate(buses):
         if period == 'x':
             continue
-        all_period, all_phase = combine_phased_rotations(all_period, all_phase, period, phase)
+        all_period, all_phase = elf.combine_phased_rotations(all_period, all_phase, period, phase)
     return all_period - all_phase
 
 
