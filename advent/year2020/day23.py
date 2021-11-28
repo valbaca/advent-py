b24031b53ad68d6c7c9c8a3a8eaf46362d20aeac
Valentin Baca <valbaca@gmail.com>
2021-11-28 15:23:18 -0800

    day 23 still too slow for part 2

--- advent/year2020/day23.py
+++ advent/year2020/day23.py
@@ -2,54 +2,48 @@ from advent import elf
 
 
 def rem(a, i):
-    rest, removed = a, []
+    removed = []
     i1 = i + 1
     for _ in range(3):
-        if i1 >= len(rest):
-            # slow way
-            ei = rest[i]
-            i1 = i1 % len(rest)
-            removed.append(rest[i1])
-            del rest[i1]
-            i = rest.index(ei)
-            i1 = i + 1
+        if i1 < len(a):
+            removed.append(a[i1])
+            del a[i1]
         else:
-            # fast way
-            removed.append(rest[i1])
-            del rest[i1]
-        # i1 = (i+1) % len(rest)
-        # e = a[(i + x) % len(a)]
-        # removed.append(rest[i1])
-        # del e[i1]
-        # rest.remove(e)
-    return rest, removed
+            removed.append(a[0])
+            del a[0]
+    return a, removed
 
 
-def find_insert_pos(rest, curr):
+def max_with_index(a):
+    mx, mi = a[0], 0
+    for i, e in enumerate(a):
+        if e > mx:
+            mx, mi = e, i
+    return mx, mi
+
+
+def find_dest(rest, three, curr):
     seek = curr - 1
-    try:
-        return rest.index(seek)
-    except ValueError:
-        # slow way
-        mn = min(rest)
-        while seek >= mn:
-            if seek in rest:
-                return rest.index(seek)
-            seek -= 1
-        mx = max(rest)
-        return rest.index(mx)
+    while seek in three:
+        seek -= 1
+    if seek < 1:
+        return max_with_index(rest)[1]
+    return rest.index(seek)
 
 
 def move(a, ci):
     curr = a[ci]
     rest, three = rem(a, ci)
-    insert_pos = find_insert_pos(rest, curr)
-    new_a = rest[:insert_pos + 1] + three + rest[insert_pos + 1:]
-    new_ci = (new_a.index(curr) + 1) % len(new_a)
-    return new_a, new_ci
+    dest1 = find_dest(rest, three, curr) + 1
+    rest[dest1:dest1] = three
+    if dest1 <= ci:
+        new_ci = (rest.index(curr) + 1) % len(rest)
+    else:
+        new_ci = ci + 1
+    return rest, new_ci
 
 
-def print_soln(a):
+def print_soln_p1(a):
     a1 = a.index(1)
     a2 = a1 + 1 % len(a)
     ax = a[a2:] + a[:a1]
@@ -61,8 +55,7 @@ def part1(inp, moves):
     ci = 0
     for i in range(moves):
         a, ci = move(a, ci)
-    return print_soln(a)
-    # code here
+    return print_soln_p1(a)
 
 
 def print_soln_p2(a):
@@ -75,21 +68,23 @@ def print_soln_p2(a):
 
 def part2(inp, moves):
     a = list(map(elf.safe_atoi, inp))
-    for x in range(max(a) + 1, 1000000):
+    for x in range(max(a) + 1, 1000001):
         a.append(x)
     ci = 0
     for i in range(moves):
-        # print(i, end=" ")
-        # print(f"ci={ci}, curr={a[ci]}, 1-index={a.index(1)} a={a[:100]}")
+        if i % 10000 == 0:
+            print(f"move {i}")
         a, ci = move(a, ci)
     print()
     return print_soln_p2(a)
 
 
 def main():
-    print(part1("974618352", 100))
-    # print(part2("389125467", 10000000))
-    # print(part2("974618352", 10000000))
+    ans1 = part1("974618352", 100)
+    assert (ans1 == "75893264")
+    print(ans1)
+    print(part2("389125467", 10000000))
+    print(part2("974618352", 10000000))
 
 
 if __name__ == '__main__':
