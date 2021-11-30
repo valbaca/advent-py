9d9f1d4d8e3b326f624df1e79188d32d1f9b1e3f
Valentin Baca <valbaca@gmail.com>
2021-11-29 20:13:36 -0800

    Setup year2021

---
+++ advent/year2021/day0.py
@@ -0,0 +1,22 @@
+from advent import elf
+
+
+def part1(lines):
+    ...
+
+
+def part2(lines):
+    ...
+
+
+def main():
+    test_lines = elf.read_lines(__file__, test=True)
+    print("Part 1 (test):", part1(test_lines))
+
+    lines = elf.read_lines(__file__)
+    print("Part 1:", part1(lines))
+    print("Part 2:", part2(lines))
+
+
+if __name__ == '__main__':
+    main()
