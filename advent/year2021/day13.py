b2887d4803c8fb69a76dac1c3369ec992d477a87
Valentin Baca <valbaca@gmail.com>
2022-07-31 20:38:19 -0700

    Day 13 TIL

--- advent/year2021/day13.py
+++ advent/year2021/day13.py
@@ -1,5 +1,12 @@
 from advent import elf
 
+"""
+TIL: like a lot of advent problems, best to avoid an actual grid until you need it.
+Use a list/set of points instead.
+Also, using `s in line` is Just Nice and straightforward (compared to .contains or .includes).
+It reads more like how you would describe
+"""
+
 
 def main():
     test_lines = elf.read_lines(__file__, test=True)
@@ -13,13 +20,14 @@ def main():
     print("Part 2:")
     print(part2(lines))
 
+
 def parse(lines):
     grid = []
     dirs = []
     for line in lines:
         if "," in line:
             xy = line.split(",")
-            x,y = xy
+            x, y = xy
             grid.append([int(x), int(y)])
         elif "along" in line:
             _, _, xyz = line.split(" ")
@@ -27,13 +35,15 @@ def parse(lines):
             dirs.append((xy, int(z)))
     return grid, dirs
 
+
 def fold_dot(x, y, x_or_y, z):
     if x_or_y == "x":
-        return [z - abs(z-x), y]
+        return [z - abs(z - x), y]
     elif x_or_y == "y":
-        return [x, z - abs(z-y)]
+        return [x, z - abs(z - y)]
     else:
         raise Exception("bad xy")
+
 
 def fold(grid, dir):
     x_or_y, z = dir
@@ -41,6 +51,7 @@ def fold(grid, dir):
     for x, y in grid:
         out_grid.append(fold_dot(x, y, x_or_y, z))
     return out_grid
+
 
 def part1(lines):
     grid, dirs = parse(lines)
@@ -54,12 +65,13 @@ def part2(lines):
     for d in dirs:
         grid = fold(grid, d)
     grid_tuples = [tuple(dot) for dot in grid]
-    max_x = max([x for x, _ in grid_tuples])+1
-    max_y = max([y for _, y in grid_tuples])+1
+    max_x = max([x for x, _ in grid_tuples]) + 1
+    max_y = max([y for _, y in grid_tuples]) + 1
     dgrid = [['.' for _ in range(max_x)] for _ in range(max_y)]
     for x, y in grid_tuples:
         dgrid[y][x] = '#'
     return "\n".join([" ".join(row) for row in dgrid])
+
 
 if __name__ == '__main__':
     main()
