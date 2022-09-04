8ab7bfa596b93bc86c7012d0f077a28d1d70df07
Valentin Baca <valbaca@gmail.com>
2022-09-04 14:33:13 -0700

    Year 2015, Day 22 improvements

--- advent/year2015/day22.py
+++ advent/year2015/day22.py
@@ -1,5 +1,4 @@
 import math
-import sys
 from enum import Enum
 from dataclasses import dataclass
 
@@ -9,6 +8,7 @@ from dataclasses import dataclass
 # - Enums are straightforward, they have .name and .value
 # - Important part of the backtrack algorithm was cutting off never-ended paths, the "invalid" check is crucial
 # - pypy is much faster! pypy took 7.6s, python3 took 95.5s => ~11.5x faster!
+# - (Sep 2022) Improved speed even more by not casting all spells in succession; using state and clones for backtracking.
 
 backtracking = """
 def backtrack(args):
@@ -23,8 +23,6 @@ def backtrack(args):
 """
 
 hard_mode = False
-
-
 class Spell(Enum):
     MagicMissile = ("Magic Missile", 53)
     Drain = ("Drain", 73)
@@ -54,101 +52,119 @@ class Boss:
     def dead(self):
         return self.hp <= 0
 
+@dataclass
+class State:
+    you: You
+    boss: Boss
+    effects: dict
+    mana_spent: int
 
-def apply(you, boss, effects):
-    next_turn_effects = []
-    for effect in effects:
-        (es, duration) = effect
-        if es == Spell.Shield:
-            you.armor = 7
-        elif es == Spell.Poison:
+    def clone(self):
+        return State(
+            You(self.you.hp, self.you.mana, self.you.armor),
+            Boss(self.boss.hp, self.boss.dmg),
+            self.effects.copy(),
+            self.mana_spent
+        )
+
+def apply(state):
+    you, boss, effects = state.you, state.boss, state.effects
+    to_del = []
+    for sp, dur in effects.items():
+        if sp == Spell.Shield:
+            if dur == 1:
+                you.armor = 0
+            else:
+                you.armor = 7
+        elif sp == Spell.Poison:
             boss.hp -= 3
-        elif es == Spell.Recharge:
+        elif sp == Spell.Recharge:
             you.mana += 101
-
-        if duration > 1:
-            next_turn_effects.append((es, duration - 1))
-        elif es == Spell.Shield and duration == 1:
-            you.armor = 0
-    return you, boss, next_turn_effects
+        if dur == 1:
+            to_del.append(sp)
+    for del_spell in to_del:
+        del effects[del_spell]
+    for sp in effects.keys():
+        effects[sp] -= 1
+    return effects
 
 
 failure = "failure"
 stalemate = "stalemate"
 victory = "victory"
 
-def battle(spells, min_found):
-    # Execute the spells in order
-    you = You(50, 500, 0)
-    boss = Boss(58, 9)
-    effects = []
-    mana_spent = 0
-    for spell in spells:
-        if hard_mode:
-            you.get_hit(1)
-            if you.dead():
-                return failure, -1
-        # each loop is one round: effects, you cast, effects again, boss hits back
-        you, boss, effects = apply(you, boss, effects)
-        if boss.dead():
-            return victory, mana_spent
-        spell_cost = spell.value[1]
-        mana_spent += spell_cost
-        if mana_spent > min_found:
-            return failure, -1  # quit while youre ahead
-        you.mana -= spell_cost
-        active_effect_spells = [e[0] for e in effects]
-        if spell in active_effect_spells or you.mana < 0:
-            return failure, -1
-        # cast spell!
-        if spell == Spell.MagicMissile:
-            boss.hp -= 4
-        elif spell == Spell.Drain:
-            you.hp += 2
-            boss.hp -= 2
-        elif spell == Spell.Shield:
-            effects.append((spell, 6))
-        elif spell == Spell.Poison:
-            effects.append((spell, 6))
-        elif spell == Spell.Recharge:
-            effects.append((spell, 5))
+# apply the spell and execute one round of combat, returns result and resulting state
+def apply_spell(state: State, spell, min_found):
+    if hard_mode:
+        state.you.get_hit(1)
+        if state.you.dead():
+            return failure, None
+    # each loop is one round: effects, you cast, effects again, boss hits back
+    state.effects = apply(state)
+    if state.boss.dead():
+        return victory, state
+    spell_cost = spell.value[1]
+    state.mana_spent += spell_cost
+    if state.mana_spent > min_found:
+        return failure, None  # quit while you're behind
+    state.you.mana -= spell_cost
+    if state.you.mana < 0 or (state.effects.get(spell) and state.effects[spell] > 0):
+        return failure, None
+    # cast spell!
+    if spell == Spell.MagicMissile:
+        state.boss.hp -= 4
+    elif spell == Spell.Drain:
+        state.you.hp += 2
+        state.boss.hp -= 2
+    elif spell == Spell.Shield:
+        state.effects[spell] = 6
+    elif spell == Spell.Poison:
+        state.effects[spell] = 6
+    elif spell == Spell.Recharge:
+        state.effects[spell] = 5
 
-        # End of your turn
-        if boss.dead():
-            return victory, mana_spent
+    # End of your turn
+    if state.boss.dead():
+        return victory, state
 
-        # Boss' turn
-        you, boss, effects = apply(you, boss, effects)
-        if boss.dead():
-            return victory, mana_spent
-        you.get_hit(boss.dmg)
-        # End of boss' turn
-        if you.dead():
-            return failure, -1
-    return stalemate, mana_spent
+    # Boss' turn
+    state.effects = apply(state)
+    if state.boss.dead():
+        return victory, state
+    state.you.get_hit(state.boss.dmg)
+    # End of boss' turn
+    if state.you.dead():
+        return failure, None
+    return stalemate, state
 
-def run(spells, min_found):
-    result, cost = battle(spells, min_found)
-    if result == failure or (result == victory and cost >= min_found):
-        return min_found
-    if result == victory and cost < min_found:
-        return cost
+def combat(state: State, min_found):
     for spell in Spell:
-        sub_min_found = run(spells + [spell], min_found)
+        result, restate = apply_spell(state.clone(), spell, min_found)
+        if result == failure or (result == victory and restate.mana_spent >= min_found):
+            continue
+        if result == victory and restate.mana_spent < min_found:
+            min_found = restate.mana_spent
+            continue
+        # else stalemate, continue depth search
+        sub_min_found = combat(restate, min_found)
         if sub_min_found < min_found:
             min_found = sub_min_found
     return min_found
 
-def part1(spells):
-    print(run([], math.inf)) 
+def part1():
+    # init_state = State(You(10, 250, 0), Boss(13, 8), {}, 0) # example
+    init_state = State( You(50, 500, 0), Boss(58, 9), {}, 0)
+    print(combat(init_state, math.inf))
+
+
 
 def main():
     global hard_mode
     print("part 1")
-    part1([])
+    part1()
     print("part 2")
     hard_mode = True
-    part1([])
+    part1()
 
 
 if __name__ == "__main__":
