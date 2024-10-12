b0c12a1c75331a4c805c758981ae047ca823e192
Val Baca <valbaca@gmail.com>
2024-10-12 12:14:52 -0700

    prep Y23

---
+++ advent/year2023/day0-min.py
@@ -0,0 +1,21 @@
+from advent import elf
+
+
+def main():
+    lines = elf.read_lines(__file__)
+    print("Part 1:")
+    print(part1(lines))
+    print("Part 2:")
+    print(part2(lines))
+
+
+def part1(lines):
+    ...
+
+
+def part2(lines):
+    ...
+
+
+if __name__ == '__main__':
+    main()
