e05c0d31b33d299ec6f476cae5aba0d25da27548
Val Baca <valbaca@gmail.com>
2025-03-27 23:03:25 -0700

    Y24 D21 done

---
+++ advent/year2024/day21.py
@@ -0,0 +1,27 @@
+from advent import elf
+
+"""See day21 solved.ipynb"""
+
+def main():
+    test_lines = elf.read_lines(__file__, test=True)
+    lines = elf.read_lines(__file__)
+    print("Part 1 (test):")
+    print(part1(test_lines))
+    print("Part 1:")
+    print(part1(lines))
+    print("Part 2 (test):")
+    print(part2(test_lines))
+    print("Part 2:")
+    print(part2(lines))
+
+
+def part1(lines):
+    ...
+
+
+def part2(lines):
+    ...
+
+
+if __name__ == '__main__':
+    main()
