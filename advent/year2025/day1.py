9d46cfa941f2c6bf3087c9a1725d4f6110d26eea
Valentin Baca <valbaca@gmail.com>
2025-12-02 17:41:36 -0800

    Y25 D1 done

--- advent/year2025/day1.py
+++ advent/year2025/day1.py
@@ -15,12 +15,39 @@ def main():
 
 
 def part1(lines):
-    ...
+    dial = 50
+    count = 0
+    for line in lines:
+        dir, val = line[0], int(line[1:])
+        dial = dial + val if dir == "R" else dial - val
+        dial  = (dial % 100)
+        if dial == 0:
+            count += 1
+    return count
+
 
 
 def part2(lines):
-    ...
-
+    dial = 50
+    count = 0
+    for line in lines:
+        prev_dial, prev_count = dial, count
+        dir, val = line[0], int(line[1:])
+        un_mod_dial = dial + val if dir == "R" else dial - val
+        rots, dial  = divmod(un_mod_dial, 100)
+        if dial == 0:
+            count += 1
+        if dir == "R":
+            count += rots
+            if dial == 0:
+                count -= 1
+        else:
+            count += (-rots)
+            if prev_dial == 0:
+                count -= 1
+        # if count != prev_count:
+            # print(f"{line} went from {prev_dial}->{dial}, {prev_count}->{count}")
+    return count
 
 if __name__ == '__main__':
     main()
